1f835b1965e31caae12455a7fa162f39b1b71b33
Coroba Show <corobonhosni@gmail.com>
2018-08-30 23:38:00 -0400

    Se emite para eliminar la restricion

--- public/js/socket-custom.js
+++ public/js/socket-custom.js
@@ -1,24 +1,46 @@
-var socket = io();
+var socket = io({reconnectionAttempts:10});
+
+
+
 socket.on("connect", function() {
   console.log("Conectado con el servidor");
 });
+
 
 socket.on("disconnect", function() {
   console.log("perdimos conecion con el servidor");
 });
 
-// Enviar informacion
-socket.emit(
-  "enviarMensaje",
-  {
-    usuario: "Fernando",
-    mensaje: "Hola Mundo"
-  },
-  function(res) {
-    console.log("Respuesta server: ", res);
-  }
-);
-
-socket.on("enviarMensaje", function(message) {
-  console.log(message);
+socket.on('idClient',function(data) {
+  console.log(data);
 });
+
+socket.on('confirmacion',function(data) {
+  console.log(data);
+  if(data.estado){
+    let cuerpo = document.querySelector('h2')
+    cuerpo.innerText = data.mensaje
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  let cuerpo = document.querySelector("body");
+
+  cuerpo.addEventListener("click", function() {
+    socket.emit("confirmacion", {
+      id: 'rIGxQZUY1vlu81RcAAAx',
+      estado: true,
+      mensaje:'Soy la otra ventana'
+    });
+  });
+});
+
+
+function abrir(){
+  miVentana = window.open('https://voragine.net/weblogs/como-pasar-variables-de-una-pagina-a-otra-en-una-web-con-php', '_blank');
+ 
+}
+
+function cerrar() {
+  miVentana.close();
+}
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -1,33 +1,15 @@
 const {io} = require('../server')
 
 io.on('connection',(client) => {
-  console.log('Usuario conectado');
+  //console.log('Usuario conectado');
 
+  client.emit('idClient',{data:client.id})
 
-  client.emit('enviarMensaje',{usuario:'admin',mensaje:'bienvenido a esta aplicacion'})
+  client.on('confirmacion',(data,callback) => {
+    
+    client.broadcast.to(data.id).emit('confirmacion',data)
 
-
-  client.on('disconnect',() => {
-    console.log('usuario desconectado');
   })
 
-
-  //Escuchar clientes
-  client.on('enviarMensaje',(messaje,callback) => {
-    console.log(messaje);
-    client.broadcast.emit('enviarMensaje',messaje)
-
-    // if (messaje.usuario) {
-    //   callback({
-    //     resp:'todo salio bien'
-    //   });
-    // }else{
-    //   callback({
-    //     resp:'todo salio MALLLL!!!!!!'
-    //   });
-    // }
-
-    // callback();
-  })
 
 });
